c15d0f818f47890458448530f2e1e38afe695f6d
Fraser McCallum <fraser@themccallums.net.nz>
2021-04-21 14:06:47 +1200

    🔥 Remove apollo server in favour of simple graphql

--- packages/app/src/pages/api/graphql.ts
+++ packages/app/src/pages/api/graphql.ts
@@ -1,23 +1,56 @@
-import { ApolloServer } from "apollo-server-micro";
-import { buildSchemaSync, buildTypeDefsAndResolvers } from "type-graphql";
+import { graphql } from "graphql";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import "reflect-metadata";
-import TestResolver from "../../graphql/resolvers/TestResolver";
+import { buildSchemaSync } from "type-graphql";
 import CalendarResolver from "../../graphql/resolvers/CalendarResolver";
+import TestResolver from "../../graphql/resolvers/TestResolver";
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: (arg0: NextApiRequest, arg1: NextApiResponse, arg2: (result: any) => void) => void
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export const appSchema = buildSchemaSync({
   validate: false,
   resolvers: [TestResolver, CalendarResolver],
 });
 
-const apolloServer = new ApolloServer({
-  playground: true,
-  schema: appSchema,
-});
+const gqlServer: NextApiHandler = async (req, res) => {
+  await runMiddleware(req, res, cors);
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
+  const query = req.body.query;
+  const params = req.body.variables;
+  const response = await graphql(appSchema, query, null, null, params);
+  return res.status(200).json(response);
 };
 
-export default apolloServer.createHandler({ path: "/api/graphql" });
+export default gqlServer;
+
+// const apolloServer = new ApolloServer({
+//   playground: true,
+//   schema: appSchema,
+// })
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+// export default apolloServer.createHandler({ path: "/api/graphql" });
